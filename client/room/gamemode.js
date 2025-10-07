657005e84922270b8ca4c0859504638d11e2d7f8
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-10-07 22:35:12 +0900

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -15,8 +15,8 @@ Room.Teams.OnAddTeam.Add(function (t) {
     t.Properties.Get("wins").Value = 0;
 });
 
-let CounterTerrorists = lib.CreateTeam("ct", { name: "Спецназ", undername: "Закладка бомбы от just_qstn" }, spez, 1);
-let Terrorists = lib.CreateTeam("t", { name: "Террористы", undername: "Закладка бомбы от just_qstn" }, ter, 2)
+let CounterTerrorists = lib.CreateTeam("ct", { name: "Спецназ", undername: "Закладка бомбы от just_qstn" }, new Basic.Color(222,184,135,0), 1);
+let Terrorists = lib.CreateTeam("t", { name: "Террористы", undername: "Закладка бомбы от just_qstn" }, new Basic.Color(64,224,208,0), 2)
 
 Room.LeaderBoard.PlayerLeaderBoardValues = [
     {
@@ -50,57 +50,4 @@ Room.LeaderBoard.PlayersWeightGetter.Set(function (p) {
     return p.Properties.Get("Kills").Value;
 });
 
-Room.Ui.TeamProp1.Value = { Team: "t", Prop: "hint" };
-Room.Ui.TeamProp2.Value = { Team: "ct", Prop: "hint" };
-Room.Ui.MainTimerId.Value = MainTimer.Id;
 
-Room.Teams.OnRequestJoinTeam.Add(function (p, t) {
-    if (p.Properties.Get("banned").Value == null)
-    {
-        p.Properties.Get("banned").Value = false;
-        p.Properties.Scores.Value = DEFAULT_MONEY;
-        if (p.Properties.Get("bomb").Value == null) p.Properties.Get("bomb").Value = EMPTY
-        p.Properties.Get("defkit").Value = EMPTY;
-    }
-    if (p.Properties.Get("isconnected").Value)
-    {
-        p.Properties.Get("banned").Value = false;
-        p.Properties.Scores.Value = DEFAULT_MONEY;
-        if (p.Properties.Get("bomb").Value == null) p.Properties.Get("bomb").Value = EMPTY
-        p.Properties.Get("defkit").Value = EMPTY;
-        p.Properties.Get("isconnected").Value = null;
-    }
-    JoinToTeam(p, t);
-    if (!p.Spawns.IsSpawned && (State.Value == STATES.Round || State.Value == STATES.Endround)) {
-        p.Spawns.Spawn();
-        p.Spawns.Despawn();
-        p.PopUp("Игра уже началась. Ждите конца игры");
-        p.Ui.Hint.Value="Игра уже началась. Ждите конца игры";
-    } else p.Spawns.Spawn();
-});
-
-Room.Players.OnPlayerConnected.Add(function (p) {
-    p.Timers.Get("join_to_team").Restart(10)
-});
-
-Room.Players.OnPlayerDisconnected.Add(function (p) {
-    if (State.Value == STATES.Round) {
-        if (GetAlivePlayersCount(Terrorists) == 0 && !IsPlanted.Value) return EndRound(CounterTerrorists);
-        if (GetAlivePlayersCount(CounterTerrorists) == 0) return EndRound(Terrorists);
-    }
-});
-
-Room.Properties.OnPlayerProperty.Add(function (c, v) {
-    if (State.Value != STATES.Clearing) {
-        switch (v.Name) {
-            case "Scores":
-                if (v.Value > MAX_MONEY) v.Value = MAX_MONEY;
-                break;
-            case "Deaths":
-                c.Player.Team.Properties.Get("hint").Value = `< Победы: ${c.Player.Team.Properties.Get("wins").Value} >\n\n< Живых: ${(GetAlivePlayersCount(c.Player.Team))} >`;
-                if (!IsPlanted.Value && GetAlivePlayersCount(c.Player.Team) <= 0) EndRound(AnotherTeam(c.Player.Team));
-                if (c.Player.Team == CounterTerrorists && IsPlanted.Value && GetAlivePlayersCount(c.Player.Team) <= 0) EndRound(Terrorists);
-                break;
-        }
-    }
-});
