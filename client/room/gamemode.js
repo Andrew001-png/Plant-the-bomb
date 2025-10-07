d581d87061ce0adf151e35f0aa8f3052aedf23d7
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-10-08 00:00:36 +0900

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -2,6 +2,21 @@ import * as Room from 'pixel_combats/room';
 import * as Basic from 'pixel_combats/basic';
 import * as lib from './library.js';
 
+const ADMIN = ["CD8BA5F2ABD9BBDA","2D2E2F256820C92","",""],
+    BANNED = [],
+    STATES = {
+        "Waiting": 0,
+        "Warmup": 1,
+        "Preround": 2,
+        "Round": 3,
+        "Endround": 4,
+        "ChangeTeams": 5,
+        "Endgame": 6,
+        "Clearing": 7
+    },
+    ENABLED = "✓",
+    EMPTY = " ";
+
 Room.BreackGraph.Damage = false;
 Room.Spawns.GetContext().Enable = false;
 
@@ -11,7 +26,7 @@ Room.Teams.OnAddTeam.Add(function (t) {
 });
 
 function RGB(r, g, b){
- return new Basic.Color(r / 255, g / 255, b / 255, 0);
+    return new Basic.Color(r / 255, g / 255, b / 255, 0);
 };
 
 let CounterTerrorists = lib.CreateTeam("ct", { name: "Спецназ", undername: "Закладка бомбы от just_qstn" }, RGB(70, 130, 180), 1);
@@ -49,4 +64,43 @@ Room.LeaderBoard.PlayersWeightGetter.Set(function (p) {
     return p.Properties.Get("Kills").Value;
 });
 
+Room.Teams.OnRequestJoinTeam.Add(function (p, t) {
+    if (p.Properties.Get("banned").Value == null)
+    {
+        if (Blacklist.Value.includes(p.Id)) {
+            BanPlayer(p);
+        }
+        else {
+            p.Properties.Get("banned").Value = false;
+            p.Properties.Scores.Value = 1000;
+            if (p.Properties.Get("bomb").Value == null) p.Properties.Get("bomb").Value = EMPTY
+            p.Properties.Get("defkit").Value = EMPTY;
+        }
+    }
+    if (p.Properties.Get("isconnected").Value)
+    {
+        p.Properties.Get("banned").Value = false;
+        p.Properties.Scores.Value = 1000;
+        if (p.Properties.Get("bomb").Value == null) p.Properties.Get("bomb").Value = EMPTY
+        p.Properties.Get("defkit").Value = EMPTY;
+        p.Properties.Get("isconnected").Value = null;
+    }
+    JoinToTeam(p, t);
+    if (!p.Spawns.IsSpawned && (State.Value == STATES.Round || State.Value == STATES.Endround)) {
+        p.Spawns.Spawn();
+        p.Spawns.Despawn();
+        p.PopUp("Игра уже началась. Ждите конца игры");
+        p.Ui.Hint.Value = "Игра уже началась, Ждите конца игры"
+    } else p.Spawns.Spawn();
+});
+
+function JoinToTeam(p, t){
+    let CT_Count = CounterTerrorists.Count - (p.Team == CounterTerrorists ? 1 : 0),
+        T_Count = Terrorists.Count - (p.Team == Terrorists ? 1 : 0);
+    if (CT_Count != T_Count) {
+        if (CT_Count < T_Count) CounterTerrorists.Add(p);
+        else if (CT_Count > T_Count) Terrorists.Add(p);
+    }
+    else t.Add(p);
+}
 
