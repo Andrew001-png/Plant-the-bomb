6ba06a8c7abd627c047337fa70d5fc98fe692f3a
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-10-07 12:22:14 +0900

    Update library.js

--- client/room/library.js
+++ client/room/library.js
@@ -1,5 +1,5 @@
 import * as Room from 'pixel_combats/room';
-import * as Basic from 'pixdl_combats/room';
+import * as Basic from 'pixel_combats/basic';
 
 export function CreateTeam(tag, name, color, spawnpoint) {
     Room.Teams.Add(tag, `<i><B><size=36>${name.name[0]}</size><size=27>${name.name.slice(1)}</size></i>\n${name.undername}</i>`, new Basic.Color(color.r, color.g, color.b, 0);
